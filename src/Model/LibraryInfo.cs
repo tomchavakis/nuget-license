2a9f885e214c1bf13d5d2c82829c46e5bd0fc8d0
devproffesional <48550900+devproffesional@users.noreply.github.com>
2020-03-06 08:57:53 +0100

    Added package version in output (#16)

--- src/Model/LibraryInfo.cs
+++ src/Model/LibraryInfo.cs
@@ -3,6 +3,7 @@ namespace NugetUtility
     public class LibraryInfo
     {
         public string PackageName { get; set; }
+        public string PackageVersion { get; set; }
         public string PackageUrl { get; set; }
         public string Description { get; set; }
         public string LicenseUrl { get; set; }
