71f539f8258257992d812929ea8ba03af23ca598
Simon Ensslen <simon.ensslen@griesser.ch>
2022-07-04 16:36:36 +0200

    correct aggressive refactoring

--- tests/targets/PackagesConfigProject/Properties/AssemblyInfo.cs
+++ tests/targets/PackagesConfigProject/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
