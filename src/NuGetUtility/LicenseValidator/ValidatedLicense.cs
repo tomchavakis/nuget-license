7fd7a39ade80aeeeabc4514d6ba5c3d26c25064a
Simon Ensslen <simon@renewedvision.com>
2022-11-02 11:25:10 +0100

    remove files that were accidentally added

--- src/NuGetUtility/LicenseValidator/ValidatedLicense.cs
+++
@@ -1,9 +0,0 @@
-using NuGetUtility.Wrapper.NuGetWrapper.Versioning;
-
-namespace NuGetUtility.LicenseValidator
-{
-    public record ValidatedLicense(string PackageId,
-        INuGetVersion PackageVersion,
-        string License,
-        LicenseInformationOrigin LicenseInformationOrigin);
-}
