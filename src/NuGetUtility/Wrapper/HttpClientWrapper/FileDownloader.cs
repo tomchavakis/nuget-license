f52bb92dfc1f85b8646d007c5654e10e43ae45e6
Simon Ensslen <sensslen@gmail.com>
2023-12-26 21:56:40 +0100

    - add rate limiting support for license downloads
    - do actually download licenses in tests
    - use different profiles for test and for app code

--- src/NuGetUtility/Wrapper/HttpClientWrapper/FileDownloader.cs
+++ src/NuGetUtility/Wrapper/HttpClientWrapper/FileDownloader.cs
@@ -1,9 +1,14 @@
-﻿namespace NuGetUtility.Wrapper.HttpClientWrapper
+﻿using System;
+
+namespace NuGetUtility.Wrapper.HttpClientWrapper
 {
     public class FileDownloader : IFileDownloader
     {
+        private readonly SemaphoreSlim _parallelDownloadLimiter = new SemaphoreSlim(10, 10);
         private readonly HttpClient _client;
         private readonly string _downloadDirectory;
+        private const int EXPONENTIAL_BACKOFF_WAIT_TIME_MILLISECONDS = 200;
+        private const int MAX_RETRIES = 5;
 
         public FileDownloader(HttpClient client, string downloadDirectory)
         {
@@ -13,10 +18,30 @@
 
         public async Task DownloadFile(Uri url, string fileName)
         {
-            await using FileStream file = File.OpenWrite(Path.Combine(_downloadDirectory, fileName));
-            await using Stream downloadStream = await _client.GetStreamAsync(url);
+            await _parallelDownloadLimiter.WaitAsync();
+            try
+            {
+                for (int i = 0; i < MAX_RETRIES; i++)
+                {
+                    await using FileStream file = File.OpenWrite(Path.Combine(_downloadDirectory, fileName));
+                    var request = new HttpRequestMessage(HttpMethod.Get, url);
 
-            await downloadStream.CopyToAsync(file);
+                    HttpResponseMessage response = await _client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+                    response.EnsureSuccessStatusCode();
+                    if (response.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
+                    {
+                        await Task.Delay((int)Math.Pow(EXPONENTIAL_BACKOFF_WAIT_TIME_MILLISECONDS, i + 1));
+                        continue;
+                    }
+                    using Stream downloadStream = await response.Content.ReadAsStreamAsync();
+
+                    await downloadStream.CopyToAsync(file);
+                }
+            }
+            finally
+            {
+                _parallelDownloadLimiter.Release();
+            }
         }
     }
 }
