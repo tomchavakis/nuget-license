ec6669f6f3f94ad9d9562b30da84a669f560f90e
Simon Ensslen <sensslen@gmail.com>
2024-05-21 17:16:09 +0200

    make sure chromedriver is properly closed...

--- tests/NuGetUtility.Test/LicenseValidator/UrlToLicenseMappingTest.cs
+++ tests/NuGetUtility.Test/LicenseValidator/UrlToLicenseMappingTest.cs
@@ -10,17 +10,16 @@ namespace NuGetUtility.Test.LicenseValidator
     [TestFixture]
     public class UrlToLicenseMappingTest
     {
+        [Parallelizable(scope: ParallelScope.All)]
         [TestCaseSource(typeof(UrlToLicenseMapping), nameof(UrlToLicenseMapping.Default))]
         public async Task License_Should_Be_Available_And_Match_Expected_License(KeyValuePair<Uri, string> mappedValue)
         {
             int retryCount = 0;
             while (true)
             {
-                var options = new ChromeOptions();
-                options.AddArguments("--no-sandbox", "--disable-dev-shm-usage", "--headless");
-                var driver = new ChromeDriver(options);
                 try
                 {
+                    using var driver = new DisposableWebDriver();
                     driver.Navigate().GoToUrl(mappedValue.Key.ToString());
 
                     await Verify(driver.FindElement(By.TagName("body")).Text).HashParameters().UseStringComparer(CompareLicense);
@@ -36,10 +35,6 @@ namespace NuGetUtility.Test.LicenseValidator
                     TestContext.Out.WriteLine($"Failed to check license for the {retryCount} time - retrying");
                     TestContext.Out.WriteLine(e);
                 }
-                finally
-                {
-                    driver.Quit();
-                }
             }
 
         }
@@ -47,6 +42,28 @@ namespace NuGetUtility.Test.LicenseValidator
         private Task<CompareResult> CompareLicense(string received, string verified, IReadOnlyDictionary<string, object> context)
         {
             return Task.FromResult(new CompareResult((!string.IsNullOrWhiteSpace(verified)) && received.Contains(verified)));
+        }
+
+        private sealed class DisposableWebDriver : IDisposable
+        {
+            private readonly IWebDriver _driver;
+
+            public DisposableWebDriver()
+            {
+                var options = new ChromeOptions();
+                options.AddArguments("--no-sandbox", "--disable-dev-shm-usage", "--headless");
+                _driver = new ChromeDriver(options);
+            }
+
+            public void Dispose()
+            {
+                _driver.Close();
+                _driver.Quit();
+                _driver.Dispose();
+            }
+
+            internal IWebElement FindElement(By by) => _driver.FindElement(by);
+            internal INavigation Navigate() => _driver.Navigate();
         }
     }
 }
